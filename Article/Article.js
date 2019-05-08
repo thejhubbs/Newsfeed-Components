326acec5f52ff79cf3b35e796406525a742ea717
Jordan Hubbard <thejhubbs@gmail.com>
2019-05-07 21:26:47 -0400

    finished expand

--- Article/Article.js
+++ Article/Article.js
@@ -7,15 +7,16 @@ class Article {
     // create a reference to the ".expandButton" class. 
     this.expandButton = domElement.querySelector('.expandButton')
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
-    // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.textContent = "Expand Button";
+
+    // Set a click handler on the expandButton reference, calling the expandArticle method.
+    this.expandButton.addEventListener('click', (e)=>{ this.expandArticle() });
     
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    this.domElement.classList.toggle("article-open");
   }
 }
 
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -10,6 +10,5 @@ const menu = document.querySelector('.menu');
 const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', (e) => {
-  console.log("click");
     toggleMenu();
 });
